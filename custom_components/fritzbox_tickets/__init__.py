c08f53a8a064bb4f0064221acd956b6e0f2d13e0
videejay <github.vcr@crimipc.de>
2026-02-07 10:12:44 +0100

    Refactor setup functions for Fritzbox tickets
    Refactor async_setup function and add setup entry and unload entry functions.

--- custom_components/fritzbox_tickets/__init__.py
+++ custom_components/fritzbox_tickets/__init__.py
@@ -1,1 +1,12 @@
-async def async_setup(hass, config): return True
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
